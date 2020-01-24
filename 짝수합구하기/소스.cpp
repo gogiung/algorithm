dbaa7af0aa46e6a8834138ade41e85dd1eb944fc
GIUNG <GIUNG@DESKTOP-OMTOC5M>
2020-01-24 14:33:26 +0900

    5

---
+++ 짝수합구하기/소스.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main() {
+
+	int n,sum=0;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (i%2 == 0) {
+			sum = sum + i;
+			
+		}
+	}
+	cout << sum;
+
+}
