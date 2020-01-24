bfa8b889eace547afb547c6f2ce4e939edb9887c
GIUNG <GIUNG@DESKTOP-OMTOC5M>
2020-01-24 14:20:51 +0900

    1

--- 5430 AC/5430 AC/소스.cpp
+++ 5430 AC/5430 AC/소스.cpp
@@ -22,20 +22,64 @@ int main() {
 		deque<int> dq;
 
 		string temp;
-		for (int i = 0; i < arr.length(); i++) 
+		for (int i = 0; i < arr.length(); i++) {
 			if (arr[i] == '[')
-		continue;
-		else if('0'<=arr[i]&&arr[i]<='9')
-		temp += arr[i];
-		else if (arr[i] == ',' || arr[i] == ']') {
-		if (!temp.empty()) {
-			dq.push_back(stoi(temp));
-			temp.clear();
+				continue;
+			else if ('0' <= arr[i] && arr[i] <= '9')
+				temp += arr[i];
+			else if (arr[i] == ',' || arr[i] == ']') {
+				if (!temp.empty()) {
+					dq.push_back(stoi(temp));
+					temp.clear();
+				}
+			}
 		}
-}
+		bool print = true;
+		bool left = true;
 
-		else {
+		for (int i = 0; i < func.length(); i++){
+			if (func[i] == 'R')
+				left = !left;
 
-}
+			else {
+				if (dq.empty()) {
+					print = false;
+					cout << "error\n";
+					break;
+				}
+				else
+					if (left)
+						dq.pop_front();
+					else
+						dq.pop_back();
+			}
+			}
+
+		if (print)
+		{
+			if (left)
+			{
+				cout << "[";
+				while (!dq.empty()) {
+					cout << dq.front();
+					dq.pop_front();
+					if (!dq.empty())
+						cout << ",";
+				}
+				cout << "]\n";
+			}
+			else {
+				cout << "[";
+				while (!dq.empty())
+				{
+					cout << dq.back();
+					dq.pop_back();
+					if (!dq.empty())
+						cout << ",";
+				}
+				cout << "]\n";
+			}
+		}
 	}
+	return 0;
 }
---
+++ 정수1개출력/정수1개출력/소스.cpp
@@ -0,0 +1,17 @@
+//#include<stdio.h>
+//
+//int main() {
+//	int a;
+//	scanf_s("%d", & a);
+//	printf("%d", a);
+//}
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a;
+	cin >>  a;
+	cout << a ;
+}
